389e03aaacb43d6f78404e2b3d4c74592866eb9d
nousheenFatima <necterflower@gmail.com>
2024-10-27 11:47:36 -0700

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=' bg-purple-300 '>
+        <h1 className='m-3'>About Us</h1> <br />
+        <p className='m-3'>Welcome to Sweet Tooth, where the art of baking meets passion and tradition! Established in 1934, our bakery has been serving up freshly baked delights that warm the heart and satisfy the soul. Nestled in the heart of Karachi, we’ve become a local favorite for those who appreciate the rich aromas and authentic flavors that only a dedicated, family-run bakery can provide.</p> <br /><br />
+        <h2 className='m-3'>Our Story</h2> <br />
+        <p className='m-3'>What began as a small, cozy shop has grown into a beloved gathering place for friends, family, and food lovers. Sweet Tooth was founded by Abdullah , whose love for baking was inspired by cherished family recipes passed down through generations. Today, we continue to honor those traditions while embracing new, innovative techniques that keep our menu exciting and fresh.</p> <br /><br />
+        <h2 className='m-3'>What Makes Us Special</h2> <br />
+        <p className='m-3'>At Sweet Tooth, we believe that great baking starts with the finest ingredients. That’s why we use only the freshest, locally-sourced produce, organic flour, and premium chocolate in every loaf, pastry, and cake we create. Each of our recipes is crafted with love and care, ensuring that every bite you take is bursting with flavor. <br /><br />From our famous signature item, e.g., garlic bread, croissants, coffee cakes to our seasonal favorites, our bakers rise early each day to bring you the best that Karach has to offer. We take pride in our hand-crafted approach, allowing us to create everything from buttery, flaky pastries to delicate macarons and artisan breads.</p>
+        <br /><br />
+        <h2 className='m-3'>Our Values</h2>
+        <br />
+        <p className='m-3'>Community is at the heart of everything we do. We believe that food has a unique way of bringing people together, and we are honored to be a part of your everyday moments—whether it’s a morning coffee and pastry, a birthday celebration, or a special treat to brighten your day. <br /><br />We are committed to sustainability, too. Our packaging is eco-friendly, and we work with local farmers and suppliers to reduce our carbon footprint. By supporting [Bakery Name], you’re also supporting the vibrant local economy.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-red-900 text-center text-white w-full h-[40px] items-center justify-center py-2'>&copy; 2024 Pakistan Tech Institute</div>
+  )
+}
+
+export default Footer
